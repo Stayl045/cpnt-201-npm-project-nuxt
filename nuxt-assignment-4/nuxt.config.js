b3c37f7153b6695db79c53bed34fd30b86e07cfa
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-28 12:30:17 -0700

    added storyblok module

--- nuxt-assignment-4/nuxt.config.js
+++ nuxt-assignment-4/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/tailwind.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,6 +32,8 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    // "@storyblok/nuxt"
+    '@storyblok/nuxt',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
